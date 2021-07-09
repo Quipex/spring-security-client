5076c3170887f572f455ae35c65e4161d4b4a75a
Quipex <rudas72@gmail.com>
2021-07-09 22:58:39 +0300

    hotfix

--- src/entry/Home.jsx
+++ src/entry/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
     return (
         <div>
             <p>Your token:</p>
-            <pre style={{whiteSpace: 'pre-wrap'}}>{JSON.stringify(token.split(','), null, 2)}</pre>
+            <pre style={{whiteSpace: 'pre-wrap'}}>{JSON.stringify(token?.split(','), null, 2)}</pre>
             <button onClick={() => localStorage.removeItem('accessToken')}>Clear token</button>
             <hr />
             <a href="http://localhost:8082/auth/oauth2/authorize/google?redirect_uri=http://localhost:3000/oauth2/resolve">Google</a>
